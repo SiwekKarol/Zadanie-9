62259a817c1a4f3005cfab6c43b8211c657510e8
KarolSiwek <siwek.karol1992@gmail.com>
2018-09-24 22:08:01 +0200

    Zadanie z dodawanie pierwszej, trzeciej i piątej liczby.

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        numbers[0] = 101;
+        numbers[1] = 2;
+        numbers[2] = 303;
+        numbers[3] = 4;
+        numbers[4] = 505;
+
+        int suma = numbers[0] + numbers[2] + numbers[4];
+        System.out.println(suma);
+
+    }
+}
